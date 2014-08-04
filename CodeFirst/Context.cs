6511f7840eaf2d601f4e4be4fe3acccb2b9efef8
Enrico Della Monica <enricodellamonica@gmail.com>
2014-08-04 13:49:16 +0200

    first

---
+++ CodeFirst/Context.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst {
+    class Context:DbContext {
+        public DbSet<User> Users { get; set; } 
+        }
+    }
---
+++ CodeFirst/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst {
+    class Program {
+        static void Main(string[] args) {
+            using (var db=new Context())
+            {
+                var user = new User();
+                user.Name = "fabio";
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+            }
+        }
+    }
